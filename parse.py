c13ef813fca2b351ba4e67bd3203450df10eccab
Frouvie <andrewpodluzhny@gmail.com>
2024-06-15 13:43:44 +0300

    Update parse.py

--- parse.py
+++ parse.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 def parse_sites(links):
     for link in links:
         responce = get(link)
-        html = BeautifulSoup(responce.content, "html.parser")
+        html = BeautifulSoup(responce.content, "xml")
         tags = html.find_all(["p", "h1", "h2", "h3"])
 
         return tags
